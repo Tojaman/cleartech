70663a3eded0213982fac11ba4c3d2f4db5afed6
Tojaman <zzoon5020@naver.com>
2026-03-19 16:12:03 +0900

    first commit

---
+++ src/main/java/com/assignment/cleartech/CleartechApplication.java
@@ -0,0 +1,13 @@
+package com.assignment.cleartech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleartechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleartechApplication.class, args);
+	}
+
+}
